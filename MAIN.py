d74ec85015648593c804821b9613dd5cd368fd28
Muhammad Umer <74130137+Umerrawala@users.noreply.github.com>
2023-05-30 02:01:13 +0500

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,19 +1,20 @@
 # Bring in deps
 import os 
-from openai_api import API_KEY 
+
 
 import streamlit as st 
+
+API=st.text_input("Enter Your OpenAI API", type="password")
+os.environ['OPENAI_API_KEY'] = API
+
+# App framework
+st.title('🦜🔗 Essay Writer')
+prompt = st.text_input('Plug in your prompt here') 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
-
-os.environ['OPENAI_API_KEY'] = API_KEY
-
-# App framework
-st.title('🦜🔗 Essay Writer')
-prompt = st.text_input('Plug in your prompt here') 
 
 # Prompt templates
 essay_template = PromptTemplate(
@@ -24,10 +25,15 @@ essay_template = PromptTemplate(
 
 
 # Memory 
+
 essay_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 
 
-
+st.info("Error asks to enter the API key. It will disappear automatically once you enter the API Key of Openai.")
+st.success("No idea how to get your API key,  well")
+if st.button("Press Me"):
+    link = '[Youtube](https://www.youtube.com/watch?v=nafDyRsVnXU)'
+    st.markdown(link, unsafe_allow_html=True)
 # Llms
 llm = OpenAI(temperature=0.9) 
 essay_chain = LLMChain(llm=llm, prompt=essay_template, verbose=True, output_key='essay', memory=essay_memory)
@@ -45,4 +51,6 @@ if prompt:
 
     with st.expander('Essay Writing History'): 
         st.info(essay_memory.buffer)
+
+st.info("Error asks to enter the API key. It will disappear automatically once you enter the API Key of Openai.")
 
