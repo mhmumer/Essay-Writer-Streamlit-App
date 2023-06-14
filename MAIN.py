d7734bae7fa051e29fc35491da49e6e574e16329
Muhammad Umer <74130137+Umerrawala@users.noreply.github.com>
2023-06-14 14:32:50 +0500

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,15 +1,25 @@
-# Bring in deps
 import os 
-
-
 import streamlit as st 
 
-API=st.text_input("Enter Your OpenAI API", type="password")
+
+with st.sidebar:
+    API=st.text_input("Enter Your OpenAI API", type="password")
+    
+    if API:
+        st.balloons()
+        
+    st.text('Made by Muhammad Umer')
+        
 os.environ['OPENAI_API_KEY'] = API
 
 # App framework
 st.title('🦜🔗 Essay Writer')
-prompt = st.text_input('Plug in your prompt here') 
+if API:
+    prompt = st.text_input('Enter your topic here') 
+else:
+    st.warning("Open the sidebar and enter your OpenAI API key")
+
+    
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
@@ -17,40 +27,32 @@ from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
 # Prompt templates
+
+
 essay_template = PromptTemplate(
     input_variables = ['topic'], 
     template='write me an essay on the topic {topic}'
 )
 
-
-
 # Memory 
-
 essay_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 
-
-st.info("Error asks to enter the API key. It will disappear automatically once you enter the API Key of Openai.")
-st.success("No idea how to get your API key,  well")
-if st.button("Press Me"):
-    link = '[Youtube](https://www.youtube.com/watch?v=nafDyRsVnXU)'
-    st.markdown(link, unsafe_allow_html=True)
 # Llms
-llm = OpenAI(temperature=0.9) 
-essay_chain = LLMChain(llm=llm, prompt=essay_template, verbose=True, output_key='essay', memory=essay_memory)
-
-
+if API:
+    llm = OpenAI(temperature=0.9) 
+    essay_chain = LLMChain(llm=llm, prompt=essay_template, verbose=True, output_key='essay', memory=essay_memory)
 
 # Show stuff to the screen if there's a prompt
-if prompt: 
-    essay = essay_chain.run(prompt)
-    
-    
+    if prompt: 
+        essay = essay_chain.run(prompt)
+        st.write(essay) 
+        
 
-    st.write(essay) 
-    
-
-    with st.expander('Essay Writing History'): 
-        st.info(essay_memory.buffer)
-
-st.info("Error asks to enter the API key. It will disappear automatically once you enter the API Key of Openai.")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
